c45416c9db2b451c45b9085c45da8ad58de176d5
NihaFariya <nihafariya786@gmail.com>
2025-07-01 09:33:34 +0530

    Your changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
-
 import './App.css';
-
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Navbar from './Component/Navbar';
 import News from './Component/News';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-export default class  extends Component {
+export default class App extends Component {
   render() {
     return (
-      <div>
-       <Navbar/>
-       <News/>
-      </div>
-    )
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route
+            path="/"
+            element={<News pageSize={5} country="us" category="general" />}
+          />
+          <Route
+            path="/business"
+            element={<News pageSize={5} country="us" category="business" />}
+          />
+          <Route
+            path="/entertainment"
+            element={<News pageSize={5} country="us" category="entertainment" />}
+          />
+          
+          <Route
+            path="/health"
+            element={<News pageSize={5} country="us" category="health" />}
+          />
+          <Route
+            path="/science"
+            element={<News pageSize={5} country="us" category="science" />}
+          />
+          <Route
+            path="/sports"
+            element={<News pageSize={5} country="us" category="sports" />}
+          />
+        </Routes>
+      </Router>
+    );
   }
 }
--- src/Component/News.js
+++ src/Component/News.js
@@ -1,66 +1,72 @@
 import React, { Component } from 'react';
 import NewsItem from './NewsItem';
+import PropTypes from 'prop-types';
 
 export class News extends Component {
+  static defaultProps = {
+    country: 'us',
+    pageSize: 8,
+    category: 'general',
+  };
+
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  };
+
   constructor() {
     super();
-    console.log("I am constructor");
-    this.apiKey = 'f9b6a344d9bf49729d30d0c1a7e51beb'; // Move API key here
+    this.apiKey = 'f9b6a344d9bf49729d30d0c1a7e51beb';
     this.state = {
       articles: [],
       loading: false,
-      page: 1
+      page: 1,
     };
   }
 
   async componentDidMount() {
-    console.log("cdm");
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${this.apiKey}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&page=${this.state.page}&pageSize=${this.props.pageSize}&apiKey=${this.apiKey}`;
     try {
       let data = await fetch(url);
       let parsedData = await data.json();
-      console.log(parsedData);
       this.setState({ articles: parsedData.articles });
     } catch (error) {
-      console.error("Fetch error:", error);
+      console.error('Fetch error:', error);
     }
   }
 
   handlePrevClick = async () => {
     if (this.state.page <= 1) return;
-    console.log("Previous");
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&page=${this.state.page - 1}&apiKey=${this.apiKey}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&page=${this.state.page - 1}&pageSize=${this.props.pageSize}&apiKey=${this.apiKey}`;
     let data = await fetch(url);
     let parsedData = await data.json();
-    console.log(parsedData);
     this.setState({
       page: this.state.page - 1,
-      articles: parsedData.articles
+      articles: parsedData.articles,
     });
-  }
+  };
 
   handleNextClick = async () => {
-    console.log("Next");
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&page=${this.state.page + 1}&apiKey=${this.apiKey}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&page=${this.state.page + 1}&pageSize=${this.props.pageSize}&apiKey=${this.apiKey}`;
     let data = await fetch(url);
     let parsedData = await data.json();
-    console.log(parsedData);
     this.setState({
       page: this.state.page + 1,
-      articles: parsedData.articles
+      articles: parsedData.articles,
     });
-  }
+  };
 
   render() {
     return (
       <div className="container my-3">
         <h2 className="text-center">NewsHotDog - Top Headlines</h2>
         <div className="row">
-          {this.state.articles && this.state.articles.map((element) => (
+          {this.state.articles.map((element) => (
             <div className="col-md-4" key={element.url}>
               <NewsItem
-                title={element.title ? element.title.slice(0, 45) : ""}
-                description={element.description ? element.description.slice(0, 88) : ""}
+                title={element.title ? element.title.slice(0, 45) : ''}
+                description={element.description ? element.description.slice(0, 88) : ''}
                 imageUrl={element.urlToImage}
                 newsUrl={element.url}
               />
